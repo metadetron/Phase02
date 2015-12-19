19de8b5ceeb6e81be261450f6b0129628b41dc52
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-19 17:58:05 +0100

    phase 5

--- src/main/java/com/metadetron/fuse/Phase02.java
+++ src/main/java/com/metadetron/fuse/Phase02.java
@@ -1,6 +1,6 @@
 public class Phase02 {
 	public static void main(String[] args) {
-		System.out.println("It runs :-) and it's phase 2");
+		System.out.println("It runs :-) and it's phase 5");
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		FileBean obj = (FileBean) context.getBean("file");
 		obj.getName();
