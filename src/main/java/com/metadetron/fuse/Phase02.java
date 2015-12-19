936b417b2478b9e7c143064aa6d8931e7932ff49
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-19 17:56:17 +0100

    springyfied

--- src/main/java/com/metadetron/fuse/Phase02.java
+++ src/main/java/com/metadetron/fuse/Phase02.java
@@ -1,5 +1,8 @@
 public class Phase02 {
 	public static void main(String[] args) {
 		System.out.println("It runs :-) and it's phase 2");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		FileBean obj = (FileBean) context.getBean("file");
+		obj.getName();
 	}
 }
---
+++ src/main/java/com/metadetron/spring/FileBean.java
@@ -0,0 +1,20 @@
+public class FileBean {
+   private String name;
+   private Integer size;
+
+   public String getName(){
+      return name;
+   }	
+
+   public void setName(String message){
+      this.name  = message;
+   }
+
+   public Integer getSize(){
+      return size;
+   }	
+
+   public void setSize(Integer message){
+      this.size  = message;
+   }
+}
