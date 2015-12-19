38121434624a3ee5ce1ecac11e59b9c43735eae0
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-19 20:58:34 +0100

    camel java file

--- src/main/java/com/metadetron/camel/Camel01.java
+++ src/main/java/com/metadetron/camel/Camel01.java
@@ -1,5 +1,5 @@
 package com.metadetron.camel;
-import org.apache.activemq.camel.component.ActiveMQComponent;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
@@ -9,20 +9,16 @@ public class Camel01 {
     public static void main(String[] args) throws Exception {
         CamelContext context = new DefaultCamelContext();
         try {
-            context.addComponent("activemq", ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false"));
             context.addRoutes(new RouteBuilder() {
-
                 @Override
                 public void configure() throws Exception {
-                    from("activemq:queue:test.queue")
+                    from("file://C:/")
                     .to("stream:out");
                 }
             });
 
             ProducerTemplate template = context.createProducerTemplate();
             context.start();
-            template.sendBody("activemq:test.queue", "Hello World");
-            Thread.sleep(2000);
         } finally {
             context.stop();
         }
