165c2158202e8c2c0c8d983481a5576ee4e61168
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-24 00:15:36 +0100

    route in xml fix

--- src/main/java/com/metadetron/camel/Camel01.java
+++ src/main/java/com/metadetron/camel/Camel01.java
@@ -4,10 +4,13 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.RoutesDefinition;
+import java.io.InputStream;
 
 public class Camel01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Camel01!");
+        CamelContext context = new DefaultCamelContext();
         InputStream is = getClass().getResourceAsStream("camelRoute.xml");
         RoutesDefinition routes = context.loadRoutesDefinition(is);
         context.addRouteDefinitions(routes.getRoutes());
