7f6c4171cdc4f8163fd83899bd3504c188ed36f3
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-24 00:09:34 +0100

    route in xml

--- src/main/java/com/metadetron/camel/Camel01.java
+++ src/main/java/com/metadetron/camel/Camel01.java
@@ -8,23 +8,12 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class Camel01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Camel01!");
-        CamelContext context = new DefaultCamelContext();
-        try {
-            context.addRoutes(new RouteBuilder() {
-                @Override
-                public void configure() throws Exception {
-                    from("file:/home/cracoviano/tmp/tmp")
-                    .to("smtps://smtp.gmail.com:465?username=testmetadetron@gmail.com&password=test2test&debugMode=true&to=cracoviano@gmail.com&mail.smtp.starttls.enable=true&subject=Test z Camela");
-                }
-            });
-
-            ProducerTemplate template = context.createProducerTemplate();
-            System.out.println("Starting context...");
-            context.start();
-            Thread.sleep(10000);
-        } finally {
-            context.stop();
-            System.out.println("Context stopped.");
-        }
+        InputStream is = getClass().getResourceAsStream("camelRoute.xml");
+        RoutesDefinition routes = context.loadRoutesDefinition(is);
+        context.addRouteDefinitions(routes.getRoutes());
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+        System.out.println("Context stopped.");
     }
 }
