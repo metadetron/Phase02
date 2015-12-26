9a4bb321c8bf8889f2ff2e9689626779b087df75
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-26 11:24:01 +0100

    no java routing

--- src/main/java/com/metadetron/camel/Camel01.java
+++ src/main/java/com/metadetron/camel/Camel01.java
@@ -10,14 +10,7 @@ import java.io.InputStream;
 public class Camel01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Camel01!");
-        CamelContext context = new DefaultCamelContext();
-        InputStream is = Camel01.class.getResourceAsStream("/camelRoute.xml");
-        if (is == null) {
-            System.out.println("File not found in " + System.getProperty("user.dir"));
-            return;
-        }
-        RoutesDefinition routes = context.loadRoutesDefinition(is);
-        context.addRouteDefinitions(routes.getRoutes());
+        CamelContext context = new DefaultCamelContext(); // is it default?
         context.start();
         Thread.sleep(10000);
         context.stop();
