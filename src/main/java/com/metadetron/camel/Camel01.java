44aa2f1ed314570996107331bdf8623c3720e4bc
Grzegorz Swierczynski <grzegorz.swierczynski@metadetron.com>
2015-12-26 12:50:29 +0100

    spring camel context

--- src/main/java/com/metadetron/camel/Camel01.java
+++ src/main/java/com/metadetron/camel/Camel01.java
@@ -3,14 +3,14 @@ package com.metadetron.camel;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.spring.SpringCamelContext;
 import org.apache.camel.model.RoutesDefinition;
 import java.io.InputStream;
 
 public class Camel01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Camel01!");
-        CamelContext context = new DefaultCamelContext(); // is it default?
+        CamelContext context = new SpringCamelContext(); // is it default? to jednak chyba nie jest default
         context.start();
         Thread.sleep(10000);
         context.stop();
